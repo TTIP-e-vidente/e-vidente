77fd6d90ceb0b6a9a4ed87a7ba2b570271511604
Agustin Di Santo <Agusdisanto99@gmail.com>
2026-04-07 00:07:29 -0300

    refactor: Remove redundant connection of input event in Menú.gd

--- project/niveles/Menú.gd
+++ project/niveles/Menú.gd
@@ -1,9 +1,5 @@
 extends Area2D
 
-func _ready(): 
-	if not input_event.is_connected(_on_input_event):
-		input_event.connect(_on_input_event)
-	
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int):
 	if event is InputEventMouseButton:
 		visible = event.is_pressed()
