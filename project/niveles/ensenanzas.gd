87c3e74f758f79d75b9a8cbb4074504c4ec7b2b9
Margo <margarita.cortizas@gmail.com>
2026-04-10 17:45:25 -0300

    Enseñanzas a Keto
    Sumo las enseñanzas de cetogénica

--- project/niveles/ensenanzas.gd
+++ project/niveles/ensenanzas.gd
@@ -9,3 +9,13 @@ const ENSENANZA_CELIAQUIA_6 = preload("res://assets-sistema/ensenanza/ensenanza-
 const ENSENANZA_CELIAQUIA_7 = preload("res://assets-sistema/ensenanza/ensenanza-celiaquia-7.png")
 const ENSENANZA_CELIAQUIA_8 = preload("res://assets-sistema/ensenanza/ensenanza-celiaquia-8.png")
 const ENSENANZA_CELIAQUIA_9 = preload("res://assets-sistema/ensenanza/ensenanza-celiaquia-9.png")
+
+
+const ENSENANZA_VEGAN_VEGETARIANE_1 = preload("res://assets-sistema/ensenanza/ensenanza-vegan-vegetariane-1.png")
+const ENSENANZA_VEGAN_VEGETARIANE_2 = preload("res://assets-sistema/ensenanza/ensenanza-vegan-vegetariane-2.png")
+const ENSENANZA_VEGAN_VEGETARIANE_3 = preload("res://assets-sistema/ensenanza/ensenanza-vegan-vegetariane-3.png")
+const ENSENANZA_VEGAN_VEGETARIANE_4 = preload("res://assets-sistema/ensenanza/ensenanza-vegan-vegetariane-4.png")
+const ENSENANZA_VEGAN_VEGETARIANE_5 = preload("res://assets-sistema/ensenanza/ensenanza-vegan-vegetariane-5.png")
+const ENSENANZA_VEGAN_VEGETARIANE_6 = preload("res://assets-sistema/ensenanza/ensenanza-vegan-vegetariane-6.png")
+const ENSENANZA_VEGAN_VEGETARIANE_7 = preload("res://assets-sistema/ensenanza/ensenanza-vegan-vegetariane-7.png")
+const ENSENANZA_VEGAN_VEGETARIANE_8 = preload("res://assets-sistema/ensenanza/ensenanza-vegan-vegetariane-8.png")
