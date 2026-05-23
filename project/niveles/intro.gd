93afea4fb09ba4bd27c07bb8568125b96c33f80a
Margo <margarita.cortizas@gmail.com>
2026-05-23 00:21:33 -0300

    Cambios en intro
    Cambios en intro

--- project/niveles/intro.gd
+++ project/niveles/intro.gd
@@ -3,9 +3,24 @@ class_name MainMenu
 
 const MUSICA_FONDO := "res://assets-sistema/sonidos/simple-relaxing-guitar-loop-60828.mp3"
 
+@onready var jugarb: TextureButton = $MenuBar/Jugar
+@onready var opcionesb: TextureButton = $MenuBar/Opciones
+@onready var salirb: TextureButton = $MenuBar/Salir
+
 @onready var jugar: Label = $MenuBar/Jugar/Label
 @onready var opciones: Label = $MenuBar/Opciones/Label
 @onready var salir: Label = $MenuBar/Salir/Label
+
+@onready var jugari: Sprite2D = $MenuBar/Jugar/imagen
+@onready var opcionesi:Sprite2D = $MenuBar/Opciones/imagen
+@onready var saliri: Sprite2D = $MenuBar/Salir/imagen
+
+const COMO_JUGAR := preload ("res://assets-sistema/intro/como-jugar-intro-1.png")
+const JUGAR := preload("res://assets-sistema/intro/play-intro-1.png")
+const SALIR :=preload("res://assets-sistema/intro/salir-intro-1.png")
+
+
+
 @onready var buttons: Array[TextureButton] = [
 	$MenuBar/Jugar,
 	$MenuBar/Opciones,
@@ -14,11 +29,15 @@ const MUSICA_FONDO := "res://assets-sistema/sonidos/simple-relaxing-guitar-loop-
 
 
 func _ready() -> void:
+	jugarb.modulate = Color("#42785e")
 	GameSceneRouter.request_initial_scene_preload()
 	_reproducir_musica_fondo()
 	jugar.text = "Jugar"
-	opciones.text = "Como jugar?"
+	opciones.text = "Cómo jugar?"
 	salir.text = "Salir"
+	jugari.texture = JUGAR
+	opcionesi.texture = COMO_JUGAR
+	saliri.texture = SALIR
 
 
 func _on_jugar_pressed() -> void:
