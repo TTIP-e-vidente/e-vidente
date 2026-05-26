726bd5f642fc2dc51c4687aea4d3f05782382ec9
Margo <margarita.cortizas@gmail.com>
2026-05-26 18:50:11 -0300

    Finalizacion partida con contenedores

--- project/mapas/finalización_partida.gd
+++ project/mapas/finalización_partida.gd
@@ -16,7 +16,6 @@ const RUBIK_SPRAY := preload("res://fonts/RubikSprayPaint-Regular.ttf")
 @onready var mensaje: Label = $Mensaje
 @onready var audio_perfecto: AudioStreamPlayer2D = $AudioPerfecto
 @onready var audio_normal: AudioStreamPlayer2D = $AudioNormal
-@onready var titulo_nivel: Label = $TituloNivel/Label
 
 
 
@@ -26,7 +25,6 @@ const CUADRADO_2X_2 = preload("res://assets-sistema/interfaz/cuadrado-2x2.png")
 
 
 func _ready() -> void:
-	titulo_nivel.text = "Celiaquía"
 	# Iconos de los bloques de stats
 	#textura.texture = CUADRADO_2X_2
 	#textura_2.texture = CUADRADO_2X_2
