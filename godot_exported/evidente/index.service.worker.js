a5323eacfa90a8c469fd1d28a5756ec3769c0288
Margo <margarita.cortizas@gmail.com>
2026-07-11 20:45:24 -0300

    Export para itchio
    Export para deploy en itchio

--- godot_exported/evidente/index.service.worker.js
+++ godot_exported/evidente/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783731783|51763026';
+const CACHE_VERSION = '1783813416|45505343';
 /** @type {string} */
 const CACHE_PREFIX = 'Evidente-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
